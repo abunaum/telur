8f1fef72a0ec3e8df7412a667736934f1c442978
abu naum <abunaum@hotmail.com>
2022-11-10 17:06:45 +0700

    add extends css js files auth

--- app/Views/auth/template.php
+++ app/Views/auth/template.php
@@ -17,13 +17,13 @@
     <link href="https://fonts.gstatic.com" rel="preconnect">
     <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Nunito:300,300i,400,400i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
 
-    <!-- Vendor CSS Files -->
-    <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-    <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
-    <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
-    <link href="assets/vendor/quill/quill.snow.css" rel="stylesheet">
-    <link href="assets/vendor/quill/quill.bubble.css" rel="stylesheet">
-    <link href="assets/vendor/remixicon/remixicon.css" rel="stylesheet">
+    <!-- Extends CSS Files -->
+    <link href="assets/extends/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="assets/extends/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+    <link href="assets/extends/boxicons/css/boxicons.min.css" rel="stylesheet">
+    <link href="assets/extends/quill/quill.snow.css" rel="stylesheet">
+    <link href="assets/extends/quill/quill.bubble.css" rel="stylesheet">
+    <link href="assets/extends/remixicon/remixicon.css" rel="stylesheet">
 
     <!-- Template Main CSS File -->
     <link href="assets/css/style.css" rel="stylesheet">
@@ -43,10 +43,10 @@
 
     <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
 
-    <!-- Vendor JS Files -->
-    <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-    <script src="assets/vendor/quill/quill.min.js"></script>
-    <script src="assets/vendor/php-email-form/validate.js"></script>
+    <!-- Extends JS Files -->
+    <script src="assets/extends/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="assets/extends/quill/quill.min.js"></script>
+    <script src="assets/extends/php-email-form/validate.js"></script>
 
     <!-- Template Main JS File -->
     <script src="assets/js/main.js"></script>
