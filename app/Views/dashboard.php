8e79b23abe82c8f1d1e2e5623a7bc6a5b31897b7
abunaum <abunaum@hotmail.com>
2022-12-12 01:53:53 +0000

    test

--- app/Views/dashboard.php
+++ app/Views/dashboard.php
@@ -4,7 +4,7 @@
 <div class="main-content">
     <div class="section__content section__content--p30">
         <div class="container-fluid">
-            <h1 class="h3 mb-4 text-gray-800">Selamat datang di Aplikasi Legi Jaya Farm.</h1>
+            <h1 class="h3 mb-4 text-gray-800">Selamat datang di Aplikasi Legi Jaya Farm :).</h1>
         </div>
     </div>
 </div>
